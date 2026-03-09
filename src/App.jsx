5cc248c5dfd8f8f4aa13ab541141d53c6e3baddd
Padmapriyan <n.p.padmapriyan@gamil.com>
2026-03-09 14:24:39 +0530

    Adding page navigation to GA4

--- src/App.jsx
+++ src/App.jsx
@@ -22,32 +22,47 @@
 
 // export default App;
 
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import AnalyticsTracker from "./components/AnalyticsTracker";
+
+// import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
+// import Tasks from "./pages/Tasks";
+
+// function App() {
+
+//   return(
+//     <BrowserRouter>
+//       {/* Track page views */}
+//       <AnalyticsTracker />
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/tasks" element={<Tasks />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
 import AnalyticsTracker from "./components/AnalyticsTracker";
 
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Tasks from "./pages/Tasks";
-
-function App() {
+function App(){
 
   return(
     <BrowserRouter>
 
-      {/* Track page views */}
       <AnalyticsTracker />
 
       <Routes>
-
         <Route path="/" element={<Login />} />
-
         <Route path="/signup" element={<Signup />} />
-
         <Route path="/login" element={<Login />} />
-
         <Route path="/tasks" element={<Tasks />} />
-
       </Routes>
 
     </BrowserRouter>
--- src/components/AnalyticsTracker.jsx
+++ src/components/AnalyticsTracker.jsx
@@ -1,15 +1,38 @@
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// function AnalyticsTracker() {
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     if (window.gtag) {
+//       window.gtag("config", "G-DVV5TD327V", {
+//         page_path: location.pathname,
+//       });
+//     }
+//   }, [location]);
+
+//   return null;
+// }
+
+// export default AnalyticsTracker;
+
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function AnalyticsTracker() {
+
   const location = useLocation();
 
   useEffect(() => {
-    if (window.gtag) {
-      window.gtag("config", "G-DVV5TD327V", {
-        page_path: location.pathname,
-      });
-    }
+
+    window.dataLayer = window.dataLayer || [];
+
+    window.dataLayer.push({
+      event: "page_view",
+      page_path: location.pathname
+    });
+
   }, [location]);
 
   return null;
